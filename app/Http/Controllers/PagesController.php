c063b114bca725bdbc64d7708039b180ee866085
James Heathcote <james.h@biffbangpow.com>
2019-05-19 14:37:40 +0100

    Basic pages and db config

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
+
+class PagesController extends Controller
+{
+    /**
+     * @return Factory|View
+     */
+    public function home()
+    {
+        return view('pages.home');
+    }
+
+    /**
+     * @return Factory|View
+     */
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+    /**
+     * @return Factory|View
+     */
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
